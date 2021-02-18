b0d69825440f757dcf78d402eed6e8073516d531
unimatrix2 <dnny.nis@gmail.com>
2021-02-18 16:03:24 -0300

    updated repository

--- src/repositories/db.repository.js
+++ src/repositories/db.repository.js
@@ -1,9 +1,10 @@
 import { Resumo } from '../models/Resumo.model';
 import AppError from '../errors/AppError';
+import { resumoMaker } from '../mappers/Resumo.mapper';
 
 export const storeResumo = async (obj) => {
     try {
-        const newResumo = new Resumo({...obj});
+        const newResumo = new Resumo(resumoMaker(obj));
         await newResumo.save();
         return newResumo;
     } catch (error) {
@@ -19,3 +20,12 @@ export const getResumos = async () => {
         throw new AppError({ message: error.message, type: 'Resumo-Get', status: 400 })
     }
 };
+
+export const getLatestResumo = async () => {
+    try {
+        const resumo = await Resumo.findOne().sort({ createdAt: -1 }).limit(1);
+        return resumo;
+    } catch (error) {
+        throw new AppError({ message: error.message, type: 'Resumo-GetLatest', status: 400 })
+    }
+}
