536a68f20ea58e6d2d20c8aebbf96d227c756aca
unimatrix2 <dnny.nis@gmail.com>
2021-02-18 05:41:09 -0300

    fixed & added more functions to repository

--- src/repositories/bling.repository.js
+++ src/repositories/bling.repository.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
+import json2xml from 'json2xml';
 import AppError from '../errors/AppError';
 import { dateToQueryFormat } from '../utils/dateConverter';
 
-export const getBlingDaily = () => {
-    const date = dateToQueryFormat()
-    axios.get(`${process.env.BLING_BASE_URL}${process.env.BLING_API_KEY}${process.env.BLING_QUERY}[${date} TO ${date}]`)
-        .then(data => {
-            if (data.data.retorno.erros[0].erro) {
-                throw new AppError({ message: data.data.retorno.erros[0].erro.msg, type: 'Error-Bling-Call', status: 400 })
-            }
-            console.log(data.data.retorno)
-        })
+export const getBlingDaily = async () => {
+    const date = dateToQueryFormat();
+    const data = await axios.get(`${process.env.BLING_BASE_URL}${process.env.BLING_API_KEY}${process.env.BLING_QUERY}[${date} TO ${date}]`);
+    if (data.data.retorno.erros[0].erro) {
+        throw new AppError({ message: data.data.retorno.erros[0].erro.msg, type: 'Error-Bling-Call', status: 400 })
+    }
+    return data.data;
 };
+
+export const postBling = (obj) => {
+    axios.post(`${process.env.BLING_BASE_URL}${process.env.BLING_API_KEY}&xml=${json2xml(obj)}`)
+        .then(() => console.log('Sucess!'))
+        .catch(err => console.log(err.data))
+}
