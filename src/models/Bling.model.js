15649346a5c420e47229bb94d39d44d12dc0d4ff
unimatrix2 <dnny.nis@gmail.com>
2021-02-18 14:31:20 -0300

    fixed model for validation

--- src/models/Bling.model.js
+++ src/models/Bling.model.js
@@ -2,29 +2,28 @@ import joi from 'joi';
 import AppError from '../errors/AppError';
 
 const blingSchemaValidation = {
-    pedido: {
-        cliente: {
+    pedido: joi.object().keys({
+        cliente: joi.object().keys({
             nome: joi.string().trim().max(80)
-        },
-        itens: {
-            item: [
-                {
-                    descricao: joi.string().trim().max(120),
+        }),
+        itens: joi.object().keys({
+            item: joi.array().items(joi.object().keys({
+                descricao: joi.string().trim().max(120),
                     vlr_unit: joi.number(),
                     qtde: joi.number(),
                     codigo: joi.number()
-                }
-            ]
-        },
+            }))
+        }),
+        numero: joi.number(),
         data: joi.string().max(10)
-    }
-};
+    })
+}
 
 const blingSchema = joi
     .object(blingSchemaValidation)
     .options({ abortEarly: false });
 
-export const validateBlingSchema = (req, res, nxt) => {
+export const validateBlingSchema = obj => {
     const joiValidation = blingSchema.validate(obj);
     if (joiValidation.error) {
         const error = joiValidation.error.details.reduce((acc, err) => {
@@ -37,5 +36,5 @@ export const validateBlingSchema = (req, res, nxt) => {
             status: 400
         });
     }
-    return nxt();
+    return obj;
 }
