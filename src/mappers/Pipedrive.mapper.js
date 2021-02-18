053391c7a0dd8b0c8efe753165881f1f96e523bc
unimatrix2 <dnny.nis@gmail.com>
2021-02-18 03:34:56 -0300

    added more mappers & fixed names

--- src/mappers/Pipedrive.mapper.js
+++ src/mappers/Pipedrive.mapper.js
@@ -4,23 +4,27 @@ export const pipedriveToBling = obj => {
     return {
         pedido: {
             cliente: {
-                nome: dataObj.data[0].person_name
+                nome: obj.data[0].person_name
             },
             itens: {
                 item: [
                     {
-                        descricao: dataObj.data[0].title,
-                        vlr_unit: dataObj.data[0].value,
+                        descricao: obj.data[0].title,
+                        vlr_unit: obj.data[0].value,
                         qtde: 1,
                         codigo: 1
                     }
                 ]
             },
-            data: dateToQueryFormat(new Date(dataObj.data[0].won_time))
+            data: dateToQueryFormat(new Date(obj.data[0].won_time))
         }
     }
 };
 
-export const blingToMongo = obj => {
-    
-}
+export const pipedriveDateMapper = obj => {
+    const yesterday = new Date().setDate(new Date().getDate() - 1);
+    obj.data.forEach(elem => {
+        elem.won_time = new Date(elem.won_time).getTime();
+    });
+    return obj.data.filter(elem => elem.won_time > yesterday);
+};
