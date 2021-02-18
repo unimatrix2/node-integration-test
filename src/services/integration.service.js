b18d542268a8742f31ae840ec65a762c7867213a
unimatrix2 <dnny.nis@gmail.com>
2021-02-18 16:05:18 -0300

    updated & fixed services

--- src/services/integration.service.js
+++ src/services/integration.service.js
@@ -1,9 +1,18 @@
 import AppError from '../errors/AppError';
 import { pipedriveDaily, getPipedriveWon } from '../repositories/pipedrive.repository';
 import { postBling } from '../repositories/bling.repository';
+import { storeResumo, getLatestResumo } from '../repositories/db.repository';
 
 export const integrator = async () => {
     const pipeData = await pipedriveDaily();
+    await registrar(pipeData);
     pipeData.map(elem => postBling(elem));
-    Promise.all(pipeData)
+    Promise.all(pipeData);
+}
+
+export const registrar = async data => {
+    const yesterday = new Date().setDate(new Date().getDate() - 1);
+    const latest = await getLatestResumo()
+    if (latest && latest.ref_dia.getTime() > yesterday) { return 0 }
+    await storeResumo(data);
 }
