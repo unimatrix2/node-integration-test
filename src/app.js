82e359b7cf8f721c389e0fd6e2e1f2cf85abd612
unimatrix2 <dnny.nis@gmail.com>
2021-02-18 16:02:50 -0300

    added sync routine

--- src/app.js
+++ src/app.js
@@ -8,16 +8,13 @@ dotenv.config();
 // Imports
 
 import mongoConnection from './configs/db.config';
-// import { integrator } from './services/integration.service'; import for testing
+import { integrator } from './services/integration.service'; // import for testing
 
 // Instance
 const app = express();
 // Middlewares Setup
 app.use(helmet());
 app.use(bodyParser.json());
-
-/* axios.get(`${process.env.PIPEDRIVE_BASE_URL}/deals?status=won&api_token=${process.env.PIPEDRIVE_API_TOKEN}`)
-    .then(data => console.log(data.data.data)) */ // Test Request
     
 // Routes Setup
 
@@ -27,3 +24,6 @@ app.use(bodyParser.json());
 mongoConnection(process.env.MONGODB_URI);
 // Server Start
 app.listen(process.env.PORT, () => console.log(`Listening on PORT ${process.env.PORT}`));
+
+integrator() // runs once at startup
+setInterval(() => integrator(), 86400000); // runs every day
